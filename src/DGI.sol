4ac61bae2ce71c65088da7ab893542c4097e011d
vishwajeeta <gs699542@gmail.com>
2024-12-09 20:44:22 +0530

    updated

--- src/DGI.sol
+++ src/DGI.sol
@@ -1,24 +1,23 @@
-
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.0;
-contract Counter{
-    uint public count;//unsigned integer
-    
-    //function to increment the count
-    function increment() public{
-        count=count+1;// or count++;
 
+contract Counter {
+    uint256 public count; //unsigned integer
+
+    //function to increment the count
+    function increment() public {
+        count = count + 1; // or count++;
     }
-    function decrement()public returns(uint _decrease){
+
+    function decrement() public returns (uint256 _decrease) {
         count--;
         return count;
     }
     //function to only read information from blockchain and not modify at all.
     //function to return number
-    function getCount()public view returns(uint){
+
+    function getCount() public view returns (uint256) {
         return count;
     }
-    
 }
-
--- test/DGI.t.sol
+++ test/DGI.t.sol
@@ -11,5 +11,4 @@ contract CounterTest is Test {
         counter = new Counter();
         counter.increment();
     }
-
 }
